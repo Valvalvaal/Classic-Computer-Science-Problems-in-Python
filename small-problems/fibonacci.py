f55d6cba018c2a77f2baf5c0de7e76d66e620e6e
Valeria Marin <valeriamarin193@gmail.com>
2022-10-15 16:54:00 -0500

    added automatic memoization

--- small-problems/fibonacci.py
+++ small-problems/fibonacci.py
@@ -1,8 +1,17 @@
 # Recursive approch to getting the nth value in the Fibonacci sequence
+# Using memoization to improve runtime of the algorithm:
+from functools import lru_cache
 
+test = 50
+
+
+@lru_cache(maxsize=None)  # Automatic memoization
 def fib(n: int) -> int:
 
     if n <= 1:
         return n
 
     return fib(n - 1) + fib(n - 2)
+
+
+print(f"fib({test}) is {fib(test)}")
