55ef93fbc744eee843b906c96710610efc18beb5
Valeria Marin <valeriamarin193@gmail.com>
2022-10-15 17:03:11 -0500

    Added memoization solution using dict

--- small-problems/fibonacci.py
+++ small-problems/fibonacci.py
@@ -2,13 +2,23 @@
 # Using memoization to improve runtime of the algorithm:
 from functools import lru_cache
 
+# Memoization using a dictionary
+memo = {0: 0, 1: 1}
+
+
+def fib_memo(n: int) -> int:
+    if n not in memo:
+        memo[n] = fib_memo(n - 2) + fib_memo(n - 1)
+    return memo[n]
+
+
+print(fib_memo(5))
+
 
 @lru_cache(maxsize=None)  # Automatic memoization
 def fib(n: int) -> int:
-
     if n <= 1:
         return n
-
     return fib(n - 1) + fib(n - 2)
 
 
