ecb771d70d889ac4a140e0d124b6359a39ee0953
Valeria Marin <valeriamarin193@gmail.com>
2022-10-16 11:27:31 -0500

    added compress gene function

--- small-problems/unbreakable_encription.py
+++ small-problems/unbreakable_encription.py
@@ -7,6 +7,20 @@ class CompressedGene:
         self.compress(gene)
 
     def _compress(self, gene: str) -> None:
+        self.bit_string = 1  # start with sentinel
+        print(self.bit_string)
+        for nucleotide in gene.upper():
+            self.bit_string <<= 2  # shift left two bits | * 2^2
+            if nucleotide == 'A':
+                self.bit_string |= 0b00
+            elif nucleotide == 'C':
+                self.bit_string |= 0b01
+            elif nucleotide == 'G':
+                self.bit_string |= 0b10
+            elif nucleotide == 'T':
+                self.bit_string |= 0b11
+            else:
+                raise ValueError(f"Invalid Nucleotide: {nucleotide}")
 
 
 def random_key(length: int) -> int:
