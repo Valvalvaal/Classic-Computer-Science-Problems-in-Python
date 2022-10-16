48c6d88869e5f51ff167e34cbf2e121eba576302
Valeria Marin <valeriamarin193@gmail.com>
2022-10-16 10:45:50 -0500

    Started trivial compression

--- small-problems/unbreakable_encription.py
+++ small-problems/unbreakable_encription.py
@@ -2,6 +2,13 @@ from secrets import token_bytes
 from typing import Tuple
 
 
+class CompressedGene:
+    def __init__(self, gene: str) -> None:
+        self.compress(gene)
+
+    def _compress(self, gene: str) -> None:
+
+
 def random_key(length: int) -> int:
 
     pass
