914fa1fe58c2e97328b7f9d339ce680d53aacac6
Valeria Marin <valeriamarin193@gmail.com>
2022-10-19 12:31:39 -0500

    implemented hanoi towers solution

--- small-problems/hanoi.py
+++ small-problems/hanoi.py
@@ -14,3 +14,29 @@ class Stack(Generic[T]):
 
     def __repr__(self) -> str:
         return repr(self._container)
+
+
+num_discs = 3
+
+tower1 = Stack()
+tower2 = Stack()
+tower3 = Stack()
+
+for i in range(num_discs):
+    tower1.push(i+1)  # 3 discs, 1 to 3 1 beign the biggest
+
+print(tower1)
+
+
+def hanoi(begin: Stack[int], end: Stack[int], temp: Stack[int], n: int) -> None:
+    if n == 1:
+        end.push(begin.pop())
+    else:
+        hanoi(begin, temp, end, n-1)
+        hanoi(begin, end, temp, 1)
+        hanoi(temp, end, begin, n-1)
+
+
+hanoi(tower1, tower3, tower2, num_discs)
+
+print(tower3)
