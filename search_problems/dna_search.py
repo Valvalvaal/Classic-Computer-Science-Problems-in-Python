84e8e8e9bda88b505eb4686d03ba792258ae012d
Valeria Marin <valeriamarin193@gmail.com>
2022-10-20 17:03:42 -0500

    implemented str_to_gene() function

--- search_problems/dna_search.py
+++ search_problems/dna_search.py
@@ -14,6 +14,8 @@ def str_to_gene(str_gene: str) -> Gene:
         # ignore the one or two nucleotides left if gene is not complete
          if i + 2 >= len(str_gene):
             return gene
-        gene.append((str_gene[i], str_gene[i+1], str_gene[i+2]))
+        codon = Codon: (str_gene[i], str_gene[i+1], str_gene[i+2])
+        gene.append(codon)
+        return gene
         
 
