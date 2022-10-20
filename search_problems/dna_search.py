8997a16e547993543aba02cb43b93a892df6a84b
Valeria Marin <valeriamarin193@gmail.com>
2022-10-20 17:12:27 -0500

    fixed issue with implementation

--- search_problems/dna_search.py
+++ search_problems/dna_search.py
@@ -7,15 +7,19 @@ Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
 Codon = (Nucleotide, Nucleotide, Nucleotide)
 Gene = [Codon]
 
+gene_str = "ACGTGGCTCTCTAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT"
+
 
 def str_to_gene(str_gene: str) -> Gene:
-    gene = Gene: []
+    gene: Gene = []
     for i in range(0, len(str_gene), 3):
         # ignore the one or two nucleotides left if gene is not complete
-         if i + 2 >= len(str_gene):
+        if i + 2 >= len(str_gene):
             return gene
-        codon = Codon: (str_gene[i], str_gene[i+1], str_gene[i+2])
+        codon: Codon = (
+            Nucleotide[str_gene[i]], Nucleotide[str_gene[i+1]], Nucleotide[str_gene[i+2]])
         gene.append(codon)
-        return gene
-        
+    return gene
 
+
+my_gene = str_to_gene(gene_str)
